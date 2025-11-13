c52ba746a13b73ac837bb18f4a480ff809372b6c
Delivio Roosblad <delivioroosblad@icloud.com>
2025-11-13 21:50:44 +0100

    Eerste versie chatbot, simpele ui

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import streamlit as st
+from services.service import get_antwoord
+
+st.set_page_config(page_title="Billie Bot", layout='centered')
+
+st.title("Billie Chat")
+
+prompt = st.text_input("Type je vraag: ")
+
+if st.button("Verstuur"):
+    if prompt.strip():
+        st.write("**Jij:**", prompt)
+        response = get_antwoord(prompt)
+        st.write(f"**Billie:** {response}")
+    else:
+        st.warning("Type eerst een bericht")
--- services/service.py
+++ services/service.py
@@ -1,5 +1,4 @@
 import google.generativeai as genai
-# from config.config import GEMINI_API_KEY, MODEL_VERSIE
 from dotenv import load_dotenv
 import os
 
@@ -10,7 +9,4 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 def get_antwoord(prompt):
     model = genai.GenerativeModel(os.getenv("MODEL_VERSIE"))
     response = model.generate_content(prompt)
-    print(response.text)
     return response.text
-
-get_antwoord("Leg uit wat ai is in 1 zin")
