f019da3a7f7921a2d72cbf6543de9613462e9877
robineenk-cloud <robineenk@gmail.com>
2025-11-14 10:17:37 +0100

    Update Tracking_codes.py

--- services/Tracking_codes.py
+++ services/Tracking_codes.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+def get_antwoord(prompt):
+    model = genai.GenerativeModel(os.getenv("MODEL_VERSIE"))
+    response = model.generate_content(prompt)
+    return response.text
 
 #data tracking codes 
 df = pd.read_csv('tracking_codes.csv')
